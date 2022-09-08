345934a34f78df890258545180b93e81654e0aba
felixngd <danhdat.gamedeveloper@gmail.com>
2022-09-08 10:42:59 +0700

    add state for tracking NeffosClient

---
+++ NeffosClientState.cs
@@ -0,0 +1,11 @@
+namespace NeffosCSharp
+{
+    public enum NeffosClientState
+    {
+        UnKnown = 0,
+        Offline = 1,
+        Connecting = 2,
+        Connected = 3,
+        Reconnecting = 4
+    }
+}
